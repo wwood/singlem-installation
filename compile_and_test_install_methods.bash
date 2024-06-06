219ce4c2075a31e05c80262bf37120947134dd35
Ben Woodcroft <donttrustben@gmail.com>
2024-06-06 20:22:02 +1000

    v0.18.0: Proof it installs.

--- compile_and_test_install_methods.bash
+++ compile_and_test_install_methods.bash
@@ -18,11 +18,12 @@ docker build --no-cache --progress=plain -f Dockerfile.pypi . &> pypi.build.log
 echo "Downloading test data .."
 wget --quiet https://github.com/wwood/singlem/raw/1d204f7ef1b90435a51cdadc18d37791fbff4c9b/docker/test.fna
 
-# Test docker install. Easier here than within a Dockerfile
+Test docker install. Easier here than within a Dockerfile
 echo "Testing docker install .."
+chmod g+rw . # Docker needs this to write to the mounted volume - diamond uses it as a temp directory
 bash -c "docker pull wwood/singlem:$SINGLEM_VERSION && docker run -v `pwd`:/data wwood/singlem:$SINGLEM_VERSION pipe --sequences /data/test.fna --otu-table /dev/stdout" &> docker.build.log
 
 # Test apptainer install. Too annoying to run this within a Dockerfile and simple enough here.
 echo "Testing apptainer install .."
 rm -f singlem_$SINGLEM_VERSION.sif
-bash -c 'singularity pull docker://wwood/singlem:$SINGLEM_VERSION && singularity run -B `pwd`:`pwd` singlem_$SINGLEM_VERSION.sif pipe --sequences `pwd`/test.fna --otu-table /dev/stdout' &> apptainer.build.log
+bash -c 'apptainer pull docker://wwood/singlem:$SINGLEM_VERSION && apptainer run --cleanenv -B `pwd`:`pwd` singlem_$SINGLEM_VERSION.sif pipe --sequences `pwd`/test.fna --otu-table /dev/stdout' > apptainer.build.stdout.log 2> apptainer.build.stderr.log
